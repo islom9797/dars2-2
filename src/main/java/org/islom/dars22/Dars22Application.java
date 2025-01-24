7d0dc122f936540a0bf382c36187cd71f364747f
islom9797 <rixsiyevvv@gmail.com>
2025-01-24 17:20:19 +0500

    first

---
+++ src/main/java/org/islom/dars22/Dars22Application.java
@@ -0,0 +1,13 @@
+package org.islom.dars22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dars22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dars22Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/islom/dars22/Dars22ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.islom.dars22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dars22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
